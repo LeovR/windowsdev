704ec762a985dc15f086a840e916eedbb56ed2c5
Jan Marten <jan.marten@gmx.de>
2014-10-11 14:29:53 +0200

    Download to subdirectory

--- install.bat
+++ install.bat
@@ -1,5 +1,6 @@
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "((new-object net.webclient).DownloadFile('https://raw.githubusercontent.com/LeovR/dotfiles/install/install.bat', '%Temp%\install.bat'))"
-call %Temp%\install.bat
+mkdir %Temp%\dotfiles
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "((new-object net.webclient).DownloadFile('https://raw.githubusercontent.com/LeovR/dotfiles/install/install.bat', '%Temp%\dotfiles\install.bat'))"
+call %Temp%\dotfiles\install.bat
 
 cinst clink
 cinst sudo
